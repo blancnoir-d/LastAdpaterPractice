4f8e7146eb7cd5cb15c7bc9552ad55244f2bf4b7
saeha <blancnoir.d@gamil.com>
2018-12-07 14:22:11 +0900

    String, int 등등에 관해서 list item 만들어봄. 데이터 반영된 리스트 구현 확인

---
+++ app/src/main/java/com/example/saeha/lastadaptertest/ItemData.kt
@@ -0,0 +1,5 @@
+package com.example.saeha.lastadaptertest
+
+class ItemData(var name:String, number:Int, var colorRes:Int) {
+    val number : String = number.toString() // int는 textview에 String으로 들어가야 ..
+}
--- app/src/main/java/com/example/saeha/lastadaptertest/MainActivity.kt
+++ app/src/main/java/com/example/saeha/lastadaptertest/MainActivity.kt
@@ -2,11 +2,57 @@ package com.example.saeha.lastadaptertest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import com.example.saeha.lastadaptertest.databinding.ItemMainBinding
+import com.example.saeha.lastadaptertest.databinding.ItemMainStringBinding
+import com.github.nitrico.lastadapter.LastAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val users = arrayListOf(
+            ItemData("경호",1,  R.color.colorPrimary),
+            ItemData("민수", 40,  R.color.colorPrimaryDark),
+            ItemData("진우", 50,  R.color.colorPrimary),
+            "사라",
+            "기홍")
+
+
+        rcyMain.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        LastAdapter(users,BR.itemData) // variable name itemData로 통일
+
+            // String
+            .map<String, ItemMainStringBinding>(R.layout.item_main_string){
+                onBind { it ->
+                    val position = it.adapterPosition
+                    val data = users[position] as String
+                    it.binding.apply {
+
+                    }
+                }
+                onClick {  }
+                onLongClick {  }
+            }
+
+            // ItemData 객체에 관한 부분
+            .map<ItemData,ItemMainBinding>(R.layout.item_main){
+                onBind {
+
+                }
+                onClick {
+                    val position = it.adapterPosition
+                    Toast.makeText(this@MainActivity,"position"+position.toString(),Toast.LENGTH_LONG).show()
+                }
+                onLongClick {  }
+            }
+            .into(rcyMain)
+
+
     }
 }
